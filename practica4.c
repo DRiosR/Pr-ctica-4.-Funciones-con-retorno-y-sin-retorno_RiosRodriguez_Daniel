58005b8cda2bbe23568335375f679564b1af720b
DRiosR <daniel.rios.rodriguez@uabc.edu.mx>
2023-11-13 08:46:16 -0800

    modified:   practica4.c

--- practica4.c
+++ practica4.c
@@ -1,21 +1,18 @@
 /*
-Práctica 4. Funciones con retorno y sin retorno
-Daniel Rios Rodiguez
-18 de septiembre de 2023
-1. Fibonacci sin Recursión: Crea un programa que calcule y muestre los primeros n
-términos de la serie de Fibonacci sin utilizar recursión.
-2. Fibonacci con Recursión: Crea un programa en C que calcule y muestre el término
-n de la serie de Fibonacci utilizando una función recursiva.
-3. Triángulo de Pascal: Crea un programa en C que genere y muestre las primeras n
-filas del Triángulo de Pascal.
+Nombre del archivo: practica4.c
+Autor: Daniel Rios Rodriguez
+Fecha de creación: 12 de Noviembre de 2023
+Descripción: este codigo contiene 3 opciones a realizar, te da los numeros de la sucesion fibonacci de tu eleccion,
+te saca el factorial de un numero y te genera el triangulo de pascal,.
 */
+
 #include <stdio.h>
 #include <stdlib.h>
-//caso 1
+// caso 1
 void fibonacci1(int a);
-//caso 2
+// caso 2
 void fibonacci2(int a);
-//caso 3
+// caso 3
 int factorial(int a);
 int pascal(int a, int b);
 
@@ -95,7 +92,14 @@ int main()
     }
     return 0;
 }
-
+/*
+Función: fibonacci1
+Descripción: Calcula y muestra los primeros 'a' términos de la serie de Fibonacci sin utilizar recursión.
+Parámetros:
+- a: Número de términos a imprimir.
+Valor de retorno: Ninguno (void).
+*/
+void fibonacci1(int a);
 void fibonacci1(int a)
 {
     int i, ante = 0, despu = 1, suma;
@@ -107,7 +111,13 @@ void fibonacci1(int a)
         despu = suma;
     }
 }
-
+/*
+Función: fibonacci2
+Descripción: Calcula y muestra el término 'n' de la serie de Fibonacci utilizando una función recursiva.
+Parámetros:
+- a: Número de términos a imprimir.
+Valor de retorno: Ninguno (void).
+*/
 void fibonacci2(int a)
 {
     int ante = 0, despu = 1, suma;
@@ -118,7 +128,13 @@ void fibonacci2(int a)
 
     return fibonacci2(a - 1);
 }
-
+/*
+Función: factorial
+Descripción: Calcula el factorial de un número entero.
+Parámetros:
+- a: Número entero para el cual se calculará el factorial.
+Valor de retorno: El factorial de 'a'.
+*/
 int factorial(int a)
 {
     int i, fac = 1;
@@ -128,6 +144,14 @@ int factorial(int a)
     }
     return fac;
 }
+/*
+Función: pascal
+Descripción: Calcula el coeficiente binomial (n choose k) para dos números enteros.
+Parámetros:
+- a: Número entero n.
+- b: Número entero k.
+Valor de retorno: El coeficiente binomial de 'a' y 'b'.
+*/
 int pascal(int a, int b)
 {
     return factorial(a) / (factorial(b) * factorial(a - b));
